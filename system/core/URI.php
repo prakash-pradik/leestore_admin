e489c0f3724c156098b260ad9f560799db94d4da
prakash-pradik <dontgiveupfreddie@gmail.com>
2024-09-16 19:45:19 +0530

    Fixed server issue

--- system/core/URI.php
+++ system/core/URI.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/userguide3/libraries/uri.html
  */
-public $config;
+ 
 class CI_URI {
 
 	/**
